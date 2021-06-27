5ef4adc70e5c5c3434bf26e32c88e11a65bafc44
nathaly <nathalygc@gmail.com>
2021-06-27 10:56:40 -0400

    updates to travel test

--- src/test/java/testPractices/testTravels.java
+++ src/test/java/testPractices/testTravels.java
@@ -5,10 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 import practice.Base;
 
@@ -36,15 +33,18 @@ public class testTravels extends Base {
         String expectedTitle1 = "Travel XML API Integrations Providers";
         String expectedTitle2 = "Application Test Drive";
         SoftAssert softAssert = new SoftAssert();
-        WebDriverWait wait = new WebDriverWait(driver,15);
+        WebDriverWait wait = new WebDriverWait(driver,6);
 
         // Ir a https://phptravels.com/demo/
         driver.get("https://phptravels.com/demo/");
 
         // Encontrar el elemento "product" y hacer Clic (menú superior)
-        driver.findElement(By.xpath("//div[@class='lvl-0 dropdown']//span[contains(text(),'Product')]")).click();
+        driver.findElement(By.xpath("//span[contains(text(),'Product')]")).click();
 
         // Encontrar y hacer clic en "Providers" (submenu de product)
+        //Comentario: En este caso por el href o texto del link se retornan dos elementos
+        // la diferencia basicamente entre ambos es el nombre de la clase y el div que lo contiene, me parecio
+        //mas sencillo buscar por la clase : //div[@class='featuresDropdown']//a[@href='https://phptravels.com/providers/'] no funciono
         driver.findElement(By.xpath("//div[@class='dropdown-content featuresDropdown']//a[@href='https://phptravels.com/providers/']")).click();
 
         // Verificar que el título sea Travel XML API Integrations Providers
@@ -55,6 +55,7 @@ public class testTravels extends Base {
 
         // Hacer clic en demo (menú superior)
         driver.findElement(By.xpath("//a[@href='https://phptravels.com/demo']")).click();
+        //driver.findElement(By.partialLinkText("Demo")); : no funciona
 
         // Verificar que el título sea Application Test Drive
         wait.until(ExpectedConditions.textToBePresentInElement(driver.findElement(By.id("header-title")), "Application Test Drive"));
@@ -66,7 +67,7 @@ public class testTravels extends Base {
         softAssert.assertAll();
     }
 
-    @AfterTest
+    @AfterMethod
     public void afterTest(){
         driver.quit();
     }
